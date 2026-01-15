aa03905d3e7b53a949037107c66bb550e053bd7e
Lokendra Nath <nathdeveloper25@gmail.com>
2026-01-15 12:33:26 +0530

    udpate and basic signup created

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
-import { UserModle } from "./db.js";
+import { connectDB, UserModle } from "./db.js";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+const JWT_PASS = "!3kjduk34j";
 
 app.post("/api/v1/signup", async (req, res) => {
   try {
@@ -12,7 +20,7 @@ app.post("/api/v1/signup", async (req, res) => {
     const { username, password } = req.body;
 
     // If Already have username
-    const user = await UserModle.find({ username });
+    const user = await UserModle.findOne({ username });
     if (user) {
       return res.status(403).json({ error: "User Already In the Database" });
     }
@@ -41,3 +49,9 @@ app.delete("/api/v1/content", (req, res) => {});
 
 app.post("/api/v1/brain/share", (req, res) => {});
 app.get("/api/v1/brain/:shareLink", (req, res) => {});
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server Starts On ${PORT}`);
+  });
+});

522378c5d3a535f4fe31bd570ee67c897a37ecc4
Lokendra Nath <nathdeveloper25@gmail.com>
2026-01-15 12:34:27 +0530

    create conned DB

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,15 @@
-import { model, Schema } from "mongoose";
+import mongoose, { model, Schema } from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const uri = process.env.MONGO_URI!;
+    await mongoose.connect(uri);
+    console.log("✅ MongoDB Connected Successfully");
+  } catch (error) {
+    console.log("❌ MongoDB Connection Failed", error);
+    process.exit(1);
+  }
+};
 
 const UserSchema = new Schema({
   username: { type: String, unique: true },

691f3b0b9e3d9d75446a98be3fcecbe871592bb3
Lokendra Nath <nathdeveloper25@gmail.com>
2026-01-23 11:24:11 +0530

    layout fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,24 @@
 import Header from "./components/Layout/Header";
-import Main from "./components/Layout/Main";
+import Main from "./components/Pages/Dashboard";
+import Sidebar from "./components/Layout/Sidebar";
+import Dashboard from "./components/Pages/Dashboard";
 
 const App = () => {
   return (
     <div className="bg-blue-100">
       {/* Header */}
-      <div className="mb-10">
+      <div className="mb-5">
         <Header />
       </div>
-      {/* Main */}
-      <Main />
+      <div className=" flex h-screen overflow-hidden gap-5">
+        <aside className="w-54 bg-blue-600 text-white shrink-0 overflow-y-auto rounded-tr-[50px] pt-10 px-5">
+          <Sidebar />
+        </aside>
+        {/* Main */}
+        <main>
+          <Dashboard />
+        </main>
+      </div>
     </div>
   );
 };

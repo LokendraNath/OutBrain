6ca9d5e918f59be45364f7206f8573206f0685fd
Lokendra Nath <nathdeveloper25@gmail.com>
2026-01-23 11:25:13 +0530

    update button component

--- client/src/components/UI/Button.tsx
+++ client/src/components/UI/Button.tsx
@@ -8,15 +8,15 @@ interface ButtonProps {
 
 const varientClasses = {
   primary: "bg-blue-500 text-white",
-  secondary: "bg-blue-100 text-blue-500",
+  secondary: "bg-blue-100 text-blue-600",
 };
 
-const defaultStyle = "px-4 py-2 rounded-md flex gap-2 items-center";
+const defaultStyle = "px-4 py-2 rounded-full flex gap-2 items-center";
 
 export const Button = ({ varient, text, startIcon }: ButtonProps) => {
   return (
     <button className={varientClasses[varient] + " " + defaultStyle}>
-      {startIcon} {text}
+      {startIcon} <h1 className="font-semibold">{text}</h1>
     </button>
   );
 };

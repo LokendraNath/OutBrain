c6eced479e917b57b2dd9620d01ec730f4daa69c
Lokendra Nath <nathdeveloper25@gmail.com>
2026-01-24 23:07:48 +0530

    fixed the header and sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,15 +6,15 @@ const App = () => {
   return (
     <div className="bg-blue-100">
       {/* Header */}
-      <div className="mb-5">
+      <div className="fixed mb-5 w-full">
         <Header />
       </div>
-      <div className="flex h-screen overflow-hidden gap-3">
-        <aside className="w-54 h-full bg-blue-600 text-white shrink-0 rounded-tr-[50px] pt-10 px-5">
+      <div className="flex h-screen overflow-hidden gap-3 pt-20">
+        <aside className="fixed top-20 h-full bg-blue-600 text-white shrink-0 rounded-tr-[50px] pt-10 px-5">
           <Sidebar />
         </aside>
         {/* Main */}
-        <main className="flex-1 h-full overflow-y-auto mt-10">
+        <main className="ml-54  mt-10">
           <Dashboard />
         </main>
       </div>
--- client/src/components/Pages/Dashboard.tsx
+++ client/src/components/Pages/Dashboard.tsx
@@ -4,45 +4,47 @@ import Card from "../UI/Card";
 const Dashboard = () => {
   return (
     <>
-      <div className="mb-5 flex space-x-4 pt-1 pl-6">
+      <div className="ml-5 flex gap-3 mb-3">
         <h1 className="text-2xl">AllNotes</h1> <NotebookPen size={30} />
       </div>
-      <div className="grid grid-cols-3 gap-5 px-5">
-        <Card
-          type="tweet"
-          title="Node js creater Talk About the AI Taking the jobs"
-          addedDate="34/23/3433"
-          tags={["political", "tech"]}
-          link="https://twitter.com/rough__sea/status/2013280952370573666?ref_src=twsrc%5Etfw"
-        />
-        <Card
-          type="link"
-          title="Workout Plan"
-          addedDate="34/23/3433"
-          tags={["fitness", "lifestyle"]}
-          link="hkkejrkj"
-        />
-        <Card
-          type="tags"
-          title="Example 2"
-          addedDate="34/23/3433"
-          tags={["fitness", "lifestyle"]}
-          link="hkkejrkj"
-        />
-        <Card
-          type="video"
-          title="Example 3"
-          addedDate="34/23/3433"
-          tags={["fitness", "lifestyle"]}
-          link="https://www.youtube.com/embed/DzUrMX1MbBE?si=ooKBQ0C3g0HTAcvO"
-        />
-        <Card
-          type="docs"
-          title="Example 4"
-          addedDate="34/23/3433"
-          tags={["fitness", "lifestyle"]}
-          link="hkkejrkj"
-        />
+      <div className="h-full overflow-y-auto p-3">
+        <div className="grid grid-cols-3 gap-5 px-5">
+          <Card
+            type="tweet"
+            title="Node js creater Talk About the AI Taking the jobs"
+            addedDate="34/23/3433"
+            tags={["political", "tech"]}
+            link="https://twitter.com/rough__sea/status/2013280952370573666?ref_src=twsrc%5Etfw"
+          />
+          <Card
+            type="link"
+            title="Workout Plan"
+            addedDate="34/23/3433"
+            tags={["fitness", "lifestyle"]}
+            link="hkkejrkj"
+          />
+          <Card
+            type="tags"
+            title="Example 2"
+            addedDate="34/23/3433"
+            tags={["fitness", "lifestyle"]}
+            link="hkkejrkj"
+          />
+          <Card
+            type="video"
+            title="Example 3"
+            addedDate="34/23/3433"
+            tags={["fitness", "lifestyle"]}
+            link="https://www.youtube.com/embed/DzUrMX1MbBE?si=ooKBQ0C3g0HTAcvO"
+          />
+          <Card
+            type="docs"
+            title="Example 4"
+            addedDate="34/23/3433"
+            tags={["fitness", "lifestyle"]}
+            link="hkkejrkj"
+          />
+        </div>
       </div>
     </>
   );

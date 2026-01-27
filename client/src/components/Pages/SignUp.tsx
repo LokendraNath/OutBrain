19c4d96dbe3ec898e0b86191e39bf42343fc32ce
Lokendra Nath <nathdeveloper25@gmail.com>
2026-01-28 00:12:22 +0530

    create initial signup page

--- client/src/components/Pages/SignUp.tsx
+++ client/src/components/Pages/SignUp.tsx
@@ -1,7 +1,28 @@
 import { LogIn, PencilRulerIcon } from "lucide-react";
 import { Button } from "../UI/Button";
+import { useState } from "react";
+import axios from "axios";
+import { BACKEND_URL } from "../../config";
 
 const SignUp = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = async () => {
+    try {
+      await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+        fullName,
+        email,
+        password,
+      });
+    } catch (error) {}
+
+    setFullName("");
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div className="h-screen flex gap-20 items-center justify-center bg-blue-500 text-white">
       {/* Left Side */}
@@ -24,18 +45,29 @@ const SignUp = () => {
         <div className="px-5 flex items-center flex-col w-full">
           <input
             type="text"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
             placeholder="Full Name"
             className="inputBoxStyle"
           />
-          <input type="email" placeholder="Email" className="inputBoxStyle" />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            className="inputBoxStyle"
+          />
           <input
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
             className="inputBoxStyle"
           />
           <Button
             varient="primary"
             text="SignUp"
+            onClick={handleSignUp}
             endIcon={<LogIn size={23} />}
             className="mt-3 w-full"
           />

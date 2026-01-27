8cfb886d0ded98297be83f5b23aa2d8e6d1f98dd
Lokendra Nath <nathdeveloper25@gmail.com>
2026-01-27 23:30:57 +0530

    impliment the simple route for initial phase

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,17 @@
 import Layout from "./components/Layout/Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./components/Pages/SignIn";
+import SignUp from "./components/Pages/SignUp";
 
 const App = () => {
-  return <Layout />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Layout />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 export default App;

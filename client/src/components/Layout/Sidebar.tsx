b627aeb495aeaae71a3d5dc7d5a22f5540781e20
Lokendra Nath <nathdeveloper25@gmail.com>
2026-02-08 20:03:13 +0530

    fix the user profile

--- client/src/components/Layout/Sidebar.tsx
+++ client/src/components/Layout/Sidebar.tsx
@@ -15,28 +15,31 @@ import SidebarIcon from "../UI/SidebarIcon";
 const Sidebar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <div className="flex items-center flex-col">
-      {/* Add Content Button */}
-      <Button
-        startIcon={<Plus size={25} />}
-        text="Add Content"
-        varient="secondary"
-        onClick={() => setIsModalOpen(true)}
-      />
+    <div className="flex justify-between items-center flex-col h-full pb-20">
+      <div>
+        {/* Add Content Button */}
 
-      {/* Create Content Modal */}
-      <CreateContentModal
-        isModalOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-      />
+        <Button
+          startIcon={<Plus size={25} />}
+          text="Add Content"
+          varient="secondary"
+          onClick={() => setIsModalOpen(true)}
+        />
 
-      {/* Routes */}
-      <div className="mt-6 flex flex-col space-y-3">
-        <SidebarIcon leftIcon={<Twitter size={25} />} text="Twitter" />
-        <SidebarIcon leftIcon={<Youtube size={25} />} text="Youtube" />
-        <SidebarIcon leftIcon={<File size={25} />} text="Docs" />
-        <SidebarIcon leftIcon={<Link size={25} />} text="Link" />
-        <SidebarIcon leftIcon={<TagIcon size={25} />} text="Tag" />
+        {/* Create Content Modal */}
+        <CreateContentModal
+          isModalOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+        />
+
+        {/* Routes */}
+        <div className="mt-6 flex flex-col space-y-3">
+          <SidebarIcon leftIcon={<Twitter size={25} />} text="Twitter" />
+          <SidebarIcon leftIcon={<Youtube size={25} />} text="Youtube" />
+          <SidebarIcon leftIcon={<File size={25} />} text="Docs" />
+          <SidebarIcon leftIcon={<Link size={25} />} text="Link" />
+          <SidebarIcon leftIcon={<TagIcon size={25} />} text="Tag" />
+        </div>
       </div>
       {/* User Profile */}
       <div className="flex items-center md:mt-50 gap-2 hover:bg-blue-700 p-1 px-2 rounded-2xl transition duration-200">
